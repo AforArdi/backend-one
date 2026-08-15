6bbca462c72ae1fc9c508807b394c70921aa6f7d
AforArdi <binsbro36@gmail.com>
2026-08-15 18:18:39 +0600

    cd added(1)

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   // Multi-file Prisma schema directory
-  schema: 'prisma',
+  schema: 'prisma/',
 
   migrations: {
     path: 'prisma/migrations',
